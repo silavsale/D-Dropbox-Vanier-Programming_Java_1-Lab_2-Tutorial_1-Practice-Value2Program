0ae05b9f4ff5a898f7aa812a832448cfaed5a480
Rsofku <silavsale@gmail.com>
2017-09-11 17:25:48 -0400

    First commit

---
+++ Value2Program.java
@@ -0,0 +1,11 @@
+public class Value2Program
+{
+	public static void main(String[] args)
+	{
+		int x = 10;
+		int y = 20;
+
+		System.out.println("The value of X variable is "+x);
+		System.out.println("\nThe value of Y variable is "+y);
+	}
+}
